137e3efceb753954c166d61f6034ff5af0fbfc66
Timothy Lopez <timothy.lopez38@gmail.com>
2016-07-29 14:01:57 -0600

    Sql cleanup.

--- data/www/sql/patches/4.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `trophies` SET `name` = 'Natural Hat Trick', `action` = 'natural-hat-trick' WHERE `action` = 'hat-trick';
-UPDATE `trophies` SET `name` = 'Natural Quad Trick', `action` = 'natural-quad-trick' WHERE `action` = 'quad-trick';
-ALTER TABLE `trophies` ADD `soundfile` VARCHAR(255) NOT NULL DEFAULT 'default.mp3';
-ALTER TABLE `trophies_players` ADD `created` TIMESTAMP NULL AFTER `trophy_id`;
--- data/www/sql/patches/5.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `trophies` (`name`,`description`, `action`) VALUES ('Hat Trick', '{player_name} has scored 3 goals with the same player.', 'hat-trick');
-INSERT INTO `trophies` (`name`,`description`, `action`) VALUES ('Quad Trick', '{player_name} has scored 4 goals with the same player.', 'quad-trick');
